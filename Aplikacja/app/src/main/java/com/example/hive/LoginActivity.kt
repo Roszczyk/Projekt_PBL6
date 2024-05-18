98e0a45428d09a620f08e2871c600d8487808aa0
Alicja Lakomska <alagracz@gmail.com>
2024-05-18 10:27:47 +0200

    hasło jako gwiazdki

--- Aplikacja/app/src/main/java/com/example/hive/LoginActivity.kt
+++ Aplikacja/app/src/main/java/com/example/hive/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.hive
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -24,6 +25,7 @@ class LoginActivity : AppCompatActivity() {
         usernameEditText = findViewById<EditText>(R.id.username)
         passwordEditText = findViewById<EditText>(R.id.password)
         loginButton = findViewById<Button>(R.id.button)
+        passwordEditText?.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
 
         // Obsługa logowania po kliknięciu przycisku
         loginButton?.setOnClickListener() {
