984c96ea27f1494429c1e4fd8facbf0796697865
Piotr-Sicinski <piotr.sicinski2002@gmail.com>
2024-06-07 18:55:27 +0200

    fdsf

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f k8s/ns.yml
-kubectl apply -f k8s/broker.yml
-kubectl apply -f k8s/db.yml
-kubectl apply -f k8s/all_us.yml
+kubectl apply -f k8/ns.yml
+kubectl apply -f k8/broker.yml
+kubectl apply -f k8/db.yml
+kubectl apply -f k8/all_us.yml
