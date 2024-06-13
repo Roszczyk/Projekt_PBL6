956016b3446a4bec3f186778f359518dadcedd75
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2024-06-13 23:14:41 +0200

    [Edge] Testowanie formatowania oraz wstawiania do bazy danych

--- Edge/mqtt_pub.py
+++ Edge/mqtt_pub.py
@@ -2,6 +2,8 @@ import random
 import paho.mqtt.client as mqtt
 import time
 
+payload_str = """{"f_port": 1, "device_id" : "4", "frm_payload": "AGcBEwBoRgCIBcOAGcBDQBoRQ==", "decoded_payload": {"temperature_0": 34.91, "presence_0":"0xFF", "relative_humidity_0": 24.95, "digital_in_1": true, "digital_in_2": true}, "rx_metadata": [{"gateway_ids": {"gateway_id": "test"}, "rssi": 42, "channel_rssi": 42, "snr": 4.2}], "settings": {"data_rate": {"lora": {"bandwidth": 125000, "spreading_factor": 7}}, "frequency": "868000000"},"dev_EUI": "70B3D57ED0063437", "received_at": "2024-05-10T10:56:38.669"}"""
+
 
 def random_integer():
     return random.randint(0, 100)
@@ -41,10 +43,11 @@ if __name__ == '__main__':
     # Connect to the MQTT broker
     # Replace with your MQTT broker address and port
     # client.connect("10.6.84.118", 1883)
-    client.connect("broker.hivemq.com", 1883)
+    # client.connect("broker.hivemq.com", 1883)
+    client.connect("10.141.10.72", 31111)
 
     # Publish a message to a topic
-    topic = "PAM-PBL5/your/topic"
+    topic = "PAM-PBL5-CATCHER"
     # topic = "t2"
 
     # Subscribe to the topic
@@ -55,7 +58,7 @@ if __name__ == '__main__':
     try:
         while True:
             time.sleep(2)
-            message = random_integer()
+            message = payload_str
             client.publish(topic, message)
     except KeyboardInterrupt:
         client.disconnect()
